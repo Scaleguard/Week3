57cade3a185c394e8b1dda78a37c6fa717aea3b0
Opilane TTHK <opilane@tthk.ee>
2020-09-14 14:19:55 +0300

    Week 3

---
+++ LoginFormRefactored/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LoginFormRefactored
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login, password;
+            Console.WriteLine("Login: ");
+            login = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            password = Console.ReadLine();
+
+            if(login == "Palpatine" && password == "Sith")
+            {
+                Console.WriteLine("Welcome, Chancolor");
+            }
+            else
+            {
+                Console.WriteLine("Login failed. You are not the senate");
+            }
+
+
+
+        }
+    }
+}
---
+++ Week3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Login Form
+             * login: admin
+             * password: pass1234
+             * create a program that asks user's login credentials
+             * Check if the user entered the correct login and password
+             * If the login and password are correct, display "Welcome"
+             * If the login or password are incorrect, display "try again"
+             
+             */
+            string login;
+            string password;
+            Console.WriteLine("Login: ");
+            login = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            password = Console.ReadLine();
+
+            if ((login == "admin") && (password == "pass1234"))
+            {
+                Console.WriteLine("Welcome");
+            }
+            else if ((login == "admin") && (password != "pass1234"))
+            {
+                Console.WriteLine("Your password is incorrect");
+            }
+            else if ((login != "admin") && (password == "pass1234"))
+            {
+                Console.WriteLine("Jedi scum");
+            }
+            else
+            {
+                Console.WriteLine("So it's treason then?");
+            }
+             
+
+            
+
+
+
+        }
+    }
+}
